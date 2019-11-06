a520490d6eca5b0f587828dd2aa671a840a1dd16
Mike Smith <hockeydad66@live.com>
2019-11-06 12:39:18 -0700

    started from scratch. HTML competed. Logic for initial buttons and added buttons working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,66 +1,35 @@
 
-var topics = ["cat", "dog", "football", "sport", "school"];
+// Global Variables
+var topics = ["cat", "dog", "football", "sport"];
 var apikey = "NkbmU5j3xQt3iIZwT1YNui67WVawmRX0";
 
+// function to display buttons for inital items in array
 
-function displayGif() {
-  var name = $(this).attr("data-name");
-  console.log (name);
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=" + apikey;
-
-  $.ajax ({
-    url: queryURL,
-    method: "GET",    
-  })
-  .then(function (response) {
-    var results = response.data;
-
-    for (var i=0; i<results.length; i++) {
-      var gifDiv = $("#gifImages");
-      var p = $("<p>").text("Rating: " + results[i].rating);
-      var gifImages = $("<img>");
-      gifImages.attr("src", results[i].images.fixed_height_still.url);
-      gifImages.attr("class", "gif");
-      gifImages.attr("data-animate", results[i].images.fixed_height.url);
-      gifImages.attr("data-still", results[i].images.fixed_height_still.url);
-
-      $(".image").on("click", function() {
-        var state = $(this).attr("data-state");
-        if (state=== "still") {
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("data-state", "animate");
-        }
-        else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-        }
-      })
-
-      gifDiv.prepend(p);
-      gifDiv.prepend(gifImages);
-
-      $("#gifImages").append(gifDiv);
-    }
-  });
-};
-
-function displayButtons () {
-  $("#gifButton").empty();
-  for (var i=0; i<topics.length; i++); {
+// //Create buttons from topics
+$(document).ready(function() {
+  $.each(topics, function(index, value) {
     var newBtn = $("<button>");
-    newBtn.addClass(".gif-btn");
-    newBtn.text(topics[i]);
-    newBtn.attr("data-name", topics[i]);
-    $("#gifButton").append(newBtn);
-  }
-}
+    newBtn.text(value);
+    newBtn.attr("data-search", value);
+    $("#buttonsDiv").prepend(newBtn);
+  });
 
-$("#add-input").on("click", function (event) {
+// Click event listener for new buttons
+
+$("#add-input").on("click", function () {
   event.preventDefault();
-  var topic = $("#input").val().trim();
-  console.log (topic);
-  topics.push(topic);
+  var name = $("#input").val().trim();
+  topics.push(name);
   console.log(topics);
+  $("#buttonsDiv").empty();
+  //Create new button
+  $.each(topics, function(index, value) {
+    var newBtn = $("<button>");
+    newBtn.text(value);
+    newBtn.attr("data-search", value);
+    $("#buttonsDiv").prepend(newBtn);
+  });
+
+}); 
+
 });
-displayGif();
-displayButtons();
