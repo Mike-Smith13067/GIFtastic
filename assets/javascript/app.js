70bdeed6b0b13de58a8d4f1062b79f2766c71ae4
Mike Smith <hockeydad66@live.com>
2019-11-06 17:42:55 -0700

    plugging away

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,34 +2,55 @@
 // Global Variables
 var topics = ["cat", "dog", "football", "sport"];
 var apikey = "NkbmU5j3xQt3iIZwT1YNui67WVawmRX0";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+  name + "&api_key=" + apikey + "&limit=10";
 
-// function to display buttons for inital items in array
 
-// //Create buttons from topics
-$(document).ready(function() {
-  $.each(topics, function(index, value) {
+// //Create buttons from inital topics on page load
+$(document).ready(function () {
+  $.each(topics, function (index, value) {
     var newBtn = $("<button>");
+    newBtn.addClass("gifBtn");
     newBtn.text(value);
     newBtn.attr("data-search", value);
     $("#buttonsDiv").prepend(newBtn);
   });
 
-// Click event listener for new buttons
+  // Click event listener for new buttons
+  $("#add-input").on("click", function () {
+    event.preventDefault();
+    var name = $("#input").val().trim();
+    topics.push(name);
+    console.log(topics);
+    $("#buttonsDiv").empty();
+    //Create and display new button
+    $.each(topics, function (index, value) {
+      var newBtn = $("<button>");
+      newBtn.addClass("gifBtn");
+      newBtn.text(value);
+      newBtn.attr("data-search", value);
+      $("#buttonsDiv").prepend(newBtn);
+    });
 
-$("#add-input").on("click", function () {
-  event.preventDefault();
-  var name = $("#input").val().trim();
-  topics.push(name);
-  console.log(topics);
-  $("#buttonsDiv").empty();
-  //Create new button
-  $.each(topics, function(index, value) {
-    var newBtn = $("<button>");
-    newBtn.text(value);
-    newBtn.attr("data-search", value);
-    $("#buttonsDiv").prepend(newBtn);
   });
+  // logic to pull the unamniamted GIF for button selected
+  $(document).on("cick", ".gifBtn"), function () {
+    event.preventDefault();
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      var response = response.data;
+      console.log(response);
+      // Appending GIFs to html
+      $.each(response, function(index, value) {
+        var images = $("<h1>");
+        images.addClass("images");
+        var gifRating = $("<div>");
+        gifRating.addClass("gifRatiing");
 
-}); 
+      })
+    })
+  }
 
 });
